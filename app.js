32054250c98e92d923dda3c23d402bddf2e3383a
Zachary Bennett <zbennett10@hotmail.com>
2017-02-18 22:57:47 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express(); //object that takes incoming http requests
+
+//watch for incoming requests of method GET to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+module.exports = app;
