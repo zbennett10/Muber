394e910114b195b36d10cfe8dc9ba6ad46e633b8
Zachary Bennett <zbennett10@hotmail.com>
2017-02-19 13:06:00 -0500

    added geoJson query and test

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -52,4 +52,25 @@ describe('Drivers controller', () => {
                     })
             })
     });
+
+    it('GET to /api/drivers finds drivers in a location', done => {
+        const seattleDriver = new Driver({
+            email: 'seattle@test.com',
+            geometry: {type: 'Point', coordinates: [-122.48, 47.61]}
+        });
+        const miamiDriver = new Driver({
+            email: 'miami@test.com',
+            geometry: {type: 'Point', coordinates: [-80.25, 25.79]}
+        });
+        Promise.all([seattleDriver.save(), miamiDriver.save()])
+            .then(() => {
+                request(app)
+                    .get('/api/drivers?lng=-80&lat=25')
+                    .end((err, response) => {
+                        assert(response.body.length === 1);
+                        assert(response.body[0].obj.email === 'miami@test.com');
+                        done();
+                    })
+            })
+    });
 });
